6ce9c58b6190afbf56e885e1cda25caec1db854b
Thiago Ventura Velez De Andrade <83434844+ThiagoVenturaV@users.noreply.github.com>
2025-10-28 19:54:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,12 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+    resolve: {
+    alias: {
+    '@': path.resolve(__dirname, './src'),
+  }
+}
+
         }
       }
     };
